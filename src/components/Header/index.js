243a6e0e74bb6c07c48bc8823f9e4560eeeae230
GuillaumeMORERO <boolabitt@hotmail.com>
2020-01-10 17:10:24 +0100

    background

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ export default () => {
   return <Container fluid className="header">
 
     <a href="/" className="header-image">
-      <img src="src/data/CastellasLOGO.png" alt="CastellasBand" className="header-image"/>
+      <img src="src/data/CastellasLOGO.png" alt="CastellasBand" className="header-image_logo"/>
     </a>
 
     <Navbar expand="lg" variant="dark" bg="none" className="header-barre">
@@ -18,29 +18,5 @@ export default () => {
     </Navbar>
 
   </Container>
-
-    {/* return <div className="header">
-    <div
-      className="header-barre"
-    >
-
-      <a href="/" className="header-image">
-        <img src="src/data/CastellasBand.jpg" alt="CastellasBand" className="header-image_cast"/>
-      </a>
-
-      <div className="header-barre_objet">
-        <a href="/concerts" className="header-barre_objet-lien">Concerts</a>
-      </div>
-      <div className="header-barre_objet">
-        <a href="/medias" className="header-barre_objet-lien">Medias</a>
-      </div>
-      <div className="header-barre_objet">
-        <a href="/contact" className="header-barre_objet-lien">Contact</a>
-      </div>
-    </div> 
-
-
-
-  </div>*/}
 
 };
