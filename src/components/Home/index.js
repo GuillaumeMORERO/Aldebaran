326059a8b9378d9152c38c2e871236c813978cab
GuillaumeMORERO <boolabitt@hotmail.com>
2020-01-11 18:04:46 +0100

    picture and style

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
+import { Container, Navbar } from 'react-bootstrap';
+
 import './style.scss';
 
 export default () => {
   return <div className="home">
-    
-  <p>Rock live band based in French riviera, covers and original songs + some pop tunes to play everywhere (pubs, festivals, gigs, restaurants, private nights)
 
-  Description de l’entreprise
-  Formed in 2018, Castellas aims to play live in the French Riviera (pubs, gigs, restaurants, festivals) and to record a first full length</p>
+    <img src="src/data/Repet Altherax.png" alt="Rehearsal" className="home-pic"/>
+    
+    <p>
+      Rock live band based in French riviera, covers and original songs + some pop tunes to play everywhere (pubs, festivals, gigs, restaurants private nights).
+      Formed in 2018, Castellas aims to play live in the French Riviera (pubs, gigs, restaurants, festivals) and to record a first full length
+    </p>
 
   </div>
 
