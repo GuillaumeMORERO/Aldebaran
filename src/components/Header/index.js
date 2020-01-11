b07be07170623b4d95ac3027f428f3b311245915
GuillaumeMORERO <boolabitt@hotmail.com>
2020-01-11 19:21:37 +0100

    menu button WIP

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar, Dropdown } from 'react-bootstrap';
 
 import './style.scss';
 
@@ -15,6 +15,18 @@ export default () => {
       <Navbar.Brand href="/concerts" className="header-barre_lien">Concerts</Navbar.Brand>
       <Navbar.Brand href="/medias" className="header-barre_lien">Medias</Navbar.Brand>
       <Navbar.Brand href="/contact" className="header-barre_lien">Contact</Navbar.Brand>
+
+      <Dropdown className="header-barre_dp">
+        <Dropdown.Toggle variant="none" id="dropdown-basic" className="header-barre_dp-tog">
+         Medias
+        </Dropdown.Toggle>
+      
+        <Dropdown.Menu className="header-barre_dp-men">
+          <Dropdown.Item href="#/action-1" className="header-barre_dp-it">Photos</Dropdown.Item>
+          <Dropdown.Item href="#/action-2" className="header-barre_dp-it">Videos</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
     </Navbar>
 
   </Container>
