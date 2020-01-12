9a79488153e7c19308defa568ccc6e8a4cd68846
GuillaumeMORERO <boolabitt@hotmail.com>
2020-01-12 23:04:33 +0100

    final header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -12,9 +12,8 @@ export default () => {
     </a>
 
     <Navbar expand="lg" variant="dark" bg="none" className="header-barre">
-      <Navbar.Brand href="/concerts" className="header-barre_lien">Concerts</Navbar.Brand>
-      <Navbar.Brand href="/medias" className="header-barre_lien">Medias</Navbar.Brand>
       <Navbar.Brand href="/contact" className="header-barre_lien">Contact</Navbar.Brand>
+      <Navbar.Brand href="/concerts" className="header-barre_lien">Concerts</Navbar.Brand>
 
       <Dropdown className="header-barre_dp">
         <Dropdown.Toggle variant="none" id="dropdown-basic" className="header-barre_dp-tog">
@@ -22,8 +21,8 @@ export default () => {
         </Dropdown.Toggle>
       
         <Dropdown.Menu className="header-barre_dp-men">
-          <Dropdown.Item href="#/action-1" className="header-barre_dp-it">Photos</Dropdown.Item>
-          <Dropdown.Item href="#/action-2" className="header-barre_dp-it">Videos</Dropdown.Item>
+          <Dropdown.Item href="/photos" className="header-barre_dp-it">Photos</Dropdown.Item>
+          <Dropdown.Item href="/videos" className="header-barre_dp-it">Videos</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
 
