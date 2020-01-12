e6bc8c44df1e482f438567b30be00360c99f6a07
GuillaumeMORERO <boolabitt@hotmail.com>
2020-01-12 17:03:51 +0100

    txt & responsiveness

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
-import { Container, Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 import './style.scss';
 
 export default () => {
-  return <div className="home">
+  return <Container fluid className="home">
 
     <img src="src/data/Repet Altherax.png" alt="Rehearsal" className="home-pic"/>
-    
-    <p>
-      Rock live band based in French riviera, covers and original songs + some pop tunes to play everywhere (pubs, festivals, gigs, restaurants private nights).
-      Formed in 2018, Castellas aims to play live in the French Riviera (pubs, gigs, restaurants, festivals) and to record a first full length
-    </p>
 
-  </div>
+    <div className="home-txts">
+      <p className="home-txts_txt">
+        Rock live band based in French riviera, covers and original songs + some pop tunes to play everywhere (pubs, festivals, gigs, restaurants private nights).
+      </p>
+      <p className="home-txts_txt">
+        We plays songs from many artists such as Yngwie Malmsteen, Europe, Scorpions, Metallica, Iron Maiden... And be as soft as Queen, Pink Floyd or Eagles !
+      </p>
+      <p className="home-txts_txt">
+        Formed in 2018, Castellas aims to play live in the French Riviera  and to record a first full length album.
+      </p>
+    </div>
+
+  </Container>
 
 };
