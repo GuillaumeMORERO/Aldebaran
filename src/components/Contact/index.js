4ed746602b7f333948a701432bb43ec779a78619
GuillaumeMORERO <boolabitt@hotmail.com>
2020-01-13 19:05:30 +0100

    simple but better

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import { Container, Form, Row, Col, Button, InputGroup, FormControl } from 'react-bootstrap';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -15,60 +15,34 @@ export default () => {
       <h2 className="contact-disclaimer_txt">En l'absence de Dollars ($), le groupe peut accepter des Euros.</h2>
     </div>
 
-    <Form>
+    <Form className="contact-zone">
 
-      <fieldset>
-        <Form.Group as={Row}>
-          <Form.Label as="legend" column sm={2}>
-            Motif du contact
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Check
-              type="radio"
-              label="Proposition de concert"
-              name="formHorizontalRadios"
-              id="formHorizontalRadios1"
-            />
-            <Form.Check
-              type="radio"
-              label="Contact de musicien"
-              name="formHorizontalRadios"
-              id="formHorizontalRadios2"
-            />
-            <Form.Check
-              type="radio"
-              label="autre motif"
-              name="formHorizontalRadios"
-              id="formHorizontalRadios3"
-            />
-          </Col>
-        </Form.Group>
-      </fieldset>
+      <div className="contact-zone_elm">
+        <label className="contact-zone_elm-lab">Email</label>
+        <input className="contact-zone_elm-area" type="email" name="email" placeholder="Entrez votre Email"/>
+      </div>
 
-      <Form.Group as={Row} controlId="formHorizontalEmail">
-        <Form.Label column sm={2}>
-          Email
-        </Form.Label>
-        <Col sm={10}>
-          <Form.Control type="email" placeholder="Email" />
-        </Col>
-      </Form.Group>
+      <div className="contact-zone_elm">
+        <label className="contact-zone_elm-lab">Message</label>
+        <textarea
+          className="contact-zone_elm-area"
+          name="message"
+          placeholder="Ecrivez votre message - 200 caractères max"
+          >
+        </textarea>
+      </div>
 
-      <Form.Group as={Row} controlId="formHorizontalMessage">
-        <Form.Label column sm={2}>
-          Message
-        </Form.Label>
-        <Col sm={10}>
-          <Form.Control type="text" placeholder="Message" />
-        </Col>
-      </Form.Group>
+      <div className="contact-zone_elm">
+        <button
+          className="contact-zone_elm-bouton"
+          type="button">
+          Envoyez !
+        </button>
+      </div>
 
-      <Form.Group as={Row}>
-        <Col sm={{ span: 10, offset: 2 }}>
-          <Button variant="none" type="submit" className="bouton">Envoyer</Button>
-        </Col>
-      </Form.Group>
     </Form>
   </Container>
 
 };
+
+// Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore assumenda dolorem atque, dolor optio consectetur odio sint? Modi ipsam molestiae recusandae accusamus blanditiis, rerum et facilis impedit, quo est expedita. Voluptatibus quidem perspiciatis numquam odit debitis, atque quaerat assumenda beatae quas consequuntur placeat aliquid modi fugiat quam iste excepturi odio aspernatur, laudantium explicabo asperiores ea. Corporis molestiae sit adipisci amet laborum provident beatae alias minus qui eius quas cum illo itaque laboriosam earum, dolorum veritatis libero, nihil voluptate velit esse reprehenderit! Laudantium hic ab corporis nisi dignissimos esse eligendi perferendis cumque reiciendis vero nam magnam quae minima deleniti, nihil ipsa repellat non pariatur placeat et! Autem quibusdam excepturi adipisci sapiente fugiat nostrum sit placeat, rem architecto ut consectetur voluptate magni similique odio aspernatur praesentium delectus quidem iure esse labore ea consequuntur odit. Natus deleniti debitis voluptatem culpa modi? Veniam facere magnam velit obcaecati. Sint illum doloribus rem dicta sapiente deserunt, fugiat sed dignissimos facilis ex. A, voluptatibus doloribus saepe, omnis ab non illo nobis adipisci sint unde debitis impedit nam. Tempore magnam illo quidem nihil sit dolorem ullam, placeat ut dolores maiores deleniti vero vitae fuga mollitia ipsam ab fugit aut voluptatibus optio praesentium. Commodi consectetur repellat neque ut laudantium.
