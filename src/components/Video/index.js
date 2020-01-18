bcded15e6d5847f84e11decdddf387720aed773f
GuillaumeMORERO <boolabitt@hotmail.com>
2020-01-18 20:36:40 +0100

    modal -à defaut de mieux pour l'instant-

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,16 +1,7 @@
 import React, { useState } from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
-
-import YouTube from '@u-wave/react-youtube';
-
 import { Container, Modal } from 'react-bootstrap';
 
-import { displayModal } from 'src/store/Videos/actions';
-
-// import '~video-react/dist/video-react.css';
-// @import '~video-react/styles/scss/video-react';
-// import "node_modules/video-react/dist/video-react.css";
 import './style.scss';
 
 export default () => {
@@ -20,19 +11,11 @@ export default () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const dispatch = useDispatch();
-  const { showModalVideo, videoToShow } = useSelector(state => state.videos);
-
-  const showModal = (input) => {
-    console.log('ça va moder!!', input);
-    dispatch(displayModal(input));
-  };
-
   return <Container className="video">
     <button
       type="button"
       className="video-title"
-      onClick={() => showModal('teaser')}
+      onClick={handleShow}
     >
       Teaser !!
     </button>
@@ -43,18 +26,11 @@ export default () => {
       className="video-image"
       onClick={handleShow}
     />
+    
 
     <div>
       <Modal show={show} onHide={handleClose} centered>
-        {/* <img
-          src="src/data/teaser.PNG"
-          alt="Teaser"
-        /> */}
-        <YouTube
-          className="customodal-box_player"
-          video="U_6Yv6-gH0E"
-          height="300"
-        />
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/U_6Yv6-gH0E" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </Modal>
     </div>
 
